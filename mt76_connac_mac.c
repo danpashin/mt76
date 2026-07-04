aab84ad0a98d555007eff15697e8651ef2310f11
Dan Pashin <danypashin@yandex.ru>
2026-07-04 22:11:50 +0300

    fix: Linux 7.1 compatibility

--- mt76_connac_mac.c
+++ mt76_connac_mac.c
@@ -422,10 +422,17 @@ mt76_connac2_mac_write_txwi_80211(struct mt76_dev *dev, __le32 *txwi,
 	u32 val;
 
 	if (ieee80211_is_action(fc) &&
+	#if LINUX_VERSION_CODE >= KERNEL_VERSION(7, 1, 0)
+	    skb->len >= IEEE80211_MIN_ACTION_SIZE(addba_req.capab) &&
+	    mgmt->u.action.category == WLAN_CATEGORY_BACK &&
+	    mgmt->u.action.action_code == WLAN_ACTION_ADDBA_REQ) {
+		u16 capab = le16_to_cpu(mgmt->u.action.addba_req.capab);
+	#else
 	    skb->len >= IEEE80211_MIN_ACTION_SIZE + 1 &&
 	    mgmt->u.action.category == WLAN_CATEGORY_BACK &&
 	    mgmt->u.action.u.addba_req.action_code == WLAN_ACTION_ADDBA_REQ) {
 		u16 capab = le16_to_cpu(mgmt->u.action.u.addba_req.capab);
+	#endif
 
 		txwi[5] |= cpu_to_le32(MT_TXD5_ADD_BA);
 		tid = (capab >> 2) & IEEE80211_QOS_CTL_TID_MASK;
